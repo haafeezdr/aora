64c0099e8ec0d567bf04ab0a2820c8acb57edde7
haafeezdr <haafeezdaiyab>
2024-10-27 17:41:13 +0100

    login with appwrite

--- app/(auth)/Sign-up.jsx
+++ app/(auth)/Sign-up.jsx
@@ -1,11 +1,86 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Link, router } from 'expo-router'
+ 
+import { images } from '../../constants'
+import FormField from '../../components/FormField'
+import CustomButton from '../../components/CustomButton'
+import { createUser } from '../../lib/appwrite'
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+})
+
+const [isSubmitting, setIsSubmitting] = useState(false)
+
+const submit = async () => {
+  if(!form.username || !form.email || !form.password) 
+  {
+    Alert.alert("Error","Please fill in all the fields")
+  }
+    setIsSubmitting(true);
+
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+
+      // set it to a global state
+
+      router.replace("/home")
+    } catch (error) {
+      Alert.alert("Error", error.message)
+    } finally {
+      setIsSubmitting(false);
+    }
+ 
+}
+
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="min-h-[84vh] w-full px-4 my-6 justify-center">
+          <Image source={images.logo}
+            resizeMode='contain' className="w-[115px] h-[35px]"/>
+          <Text className="text-2xl font-psemibold text-white mt-10 text-semibold">
+            Sign up to Aora</Text>
+            <FormField 
+              title="Username"
+              value={form.username}
+              handleChangeText={(e) => setForm({...form, username: e})}
+              otherStyles="mt-10"
+            />
+            <FormField 
+              title="Email"
+              value={form.email}
+              handleChangeText={(e) => setForm({...form, email: e})}
+              otherStyles="mt-7"
+              keyboardType="email-address"
+            />
+            <FormField 
+              title="Password"
+              value={form.password}
+              handleChangeText={(e) => setForm({...form, password: e})}
+              otherStyles="mt-7"
+            />
+            <CustomButton
+              title="Sign Up"
+              handlePress={submit}
+              containerStyles=" mt-7"
+              isLoading={isSubmitting} />
+
+              <View className="flex-row flex justify-center pt-5 gap-2">
+                <Text className="text-gray-100 text-lg font-pregular">Have an account already?
+                </Text>
+                <Link href="/Sign-in"
+                 className="text-secondary text-lg font-psemibold">
+                  Sign In
+                </Link>
+              </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -6,11 +6,11 @@ const AuthLayout = () => {
   return (
     <>
       <Stack>
-        <Stack.Screen name="sign-in" options={{ headerShown: false }}/>
-        <Stack.Screen name="sign-up" options={{ headerShown: false }}/>
+        <Stack.Screen name="Sign-in" options={{ headerShown: false }}/>
+        <Stack.Screen name="Sign-up" options={{ headerShown: false }}/>
       </Stack>
 
-      <StatusBar/>
+      <StatusBar backgroundColor='#161622' style='light' />
     </>
   )
 }
